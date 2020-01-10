93d3ce2191ed3998a239979d52071c3e54f513c4
fercer <iie.fercer@gmail.com>
2020-01-09 21:12:49 -0600

    Added setup file

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 import numpy as np
 from distutils.core import setup, Extension
 
-setup(name='gmf',
+setup(name='computeAUC',
       version='1.0',
-      description='Compute area under the ROC curve',
+      description='Area under the ROC curve',
       author='Fernando Cervantes',
       author_email='iie.fercer@gmail.com',
       
       ext_modules=[Extension('computeAUC', ['include/computeAUC.c'],
                              define_macros=[('BUILDING_PYTHON_MODULE',''), ('NDEBUG',)],
-                             include_dirs=[np.get_include()],
+                             include_dirs=[np.get_include()]
                              )],
       )
